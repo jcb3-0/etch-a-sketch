414425ceb6d3a31facd66beccb9461452fa5ffc5
Jason Bauzon <jasonbauzon@gmail.com>
2022-06-27 19:12:40 -0500

    get input from text field

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.querySelector('#container');
 // let gridSquare = document.createElement('div')
-// let gridRow = document.createElement('div');
-const rows = document.querySelectorAll('.row');
-// const squares = Array.from(document.querySelectorAll('.square'));
-let gridDimensions = prompt("How many squares per side (2 - 100)?");
+let gridRow = document.createElement('div');
+// const rows = document.querySelectorAll('.row');
+// const squares = document.querySelectorAll('.square');
+let gridDimensions = 0;
+const gridButton = document.querySelector('#createGrid')
 
 const createGrid = () => {
     for (let i = 0; i < gridDimensions; i++) {
@@ -17,7 +18,26 @@ const createGrid = () => {
         }
     }
 }
-createGrid();
+
+const removeGrid = () => {
+    document.querySelectorAll('.row').forEach(row => {
+        row.remove();
+    });
+    document.querySelectorAll('square').forEach(square => {
+        square.remove();
+    });
+}
+
+gridButton.addEventListener('click', () => {
+    gridDimensions = parseInt(document.querySelector('#gridDimensions').value);
+    removeGrid();
+    createGrid();
+    document.querySelectorAll('.square').forEach(square => 
+        square.addEventListener('mouseover', () => drawBlack(square)));
+    return;
+});
+
+//createGrid();
 
 function drawBlack(square) {
     square.style.backgroundColor = 'black';
@@ -26,4 +46,6 @@ function drawBlack(square) {
 // draw black squares with mouse
 document.querySelectorAll('.square').forEach(square => 
     square.addEventListener('mouseover', () => drawBlack(square)));
+
+
 
